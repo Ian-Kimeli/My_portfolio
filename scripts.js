4c83ab3e28fa4619799f9ead97248cc573682faf
Ian-Kimeli <iankimeli014@gmail.com>
2024-06-03 13:34:00 -0700

    Raw files push

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Currently empty, but ready for future enhancements
+console.log("Welcome to Riches & Kelly's Portfolio!");
+
+function changeBackgroundColor(element) {
+    element.style.backgroundColor = 'lightblue'; // Change to your desired color
+}
+
